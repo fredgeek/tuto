ec191fe7f0495058088ee8124fe6357ec491ecce
durelkenfack123 <kenfackdurel200@gmail.com>
2023-08-21 11:32:53 +0100

    ajouter une function de multiblication

--- collaborateur.py
+++ collaborateur.py
@@ -1,1 +1,11 @@
 #  fichier reserve au collaborateurs....
+# kenfack durel
+
+def table_mult(nombre):
+    for i in range(nombre):
+        print(f"{nombre} * {i} = {nombre*i}")
+        
+    return "fin"
+
+nmbr=int(input("entre un nombre : "))
+print(table_mult(nmbr))
